a0f0ece3b00d40ea064b4478b2b2adda4cc3a759
menchuk2000 <menchuk2000@gmail.com>
2025-10-20 01:12:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tape-cipher-react/',
+  base: '/tape-cipher-react/', // должно быть именно так
 })
